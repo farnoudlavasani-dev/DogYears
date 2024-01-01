bea08d233abefd6aa7e45a2b2273f35b5624aadb
farnoudlavasani-dev <lavasanifarnoud@gmail.com>
2024-01-01 17:18:43 +0330

    add

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-let userName = prompt('please enter your name');
-let userAge = prompt('please enter your age');
 
-if(userAge != null){
-let earlyYears = 10.5;
-let laterYears = userAge - 2;
-laterYears *= 4;
-let myAgeInDogYears = laterYears + earlyYears;
-userName = userName.toLowerCase();
-document.getElementById("test").innerHTML = `Your name is ${userName}. you are ${userAge} years old in human years which is ${myAgeInDogYears} years old in dog years`;
-}else{
-    document.getElementById("test").innerHTML = 'please enter valid value';
-}
+document.getElementById("userInputForm").addEventListener("submit",function(event){
+    event.preventDefault();
+    let userName = document.getElementById().value;
+    let userAge = document.getElementById().value;
+
+    if(userAge != null){
+        let earlyYears = 10.5;
+        let laterYears = userAge - 2;
+        laterYears *= 4;
+        let myAgeInDogYears = laterYears + earlyYears;
+        userName = userName.toLowerCase();
+        document.getElementById("test").innerHTML = `Your name is ${userName}. you are ${userAge} years old in human years which is ${myAgeInDogYears} years old in dog years`;
+        }else{
+            document.getElementById("test").innerHTML = 'please enter valid value';
+        }
+})
+
+
+
+
+
+
+
+
 
