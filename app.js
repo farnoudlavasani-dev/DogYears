53c670b8254d098c026398e24944906b1ec507e4
farnoudlavasani-dev <lavasanifarnoud@gmail.com>
2024-01-01 11:53:29 +0330

    add codes

---
+++ app.js
@@ -0,0 +1,9 @@
+//my human years
+let myAge = 20;
+let earlyYears = 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+let myAgeInDogYears = laterYears + earlyYears;
+let myName = 'Farnoud';
+myName = myName.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
