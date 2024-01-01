b1adaa58cbff520e1b8a9e076c13c4362e6c84f2
farnoudlavasani-dev <lavasanifarnoud@gmail.com>
2024-01-01 16:35:00 +0330

    prompt()

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-//my human years
-let myAge = 20;
+let userName = prompt('please enter your name');
+let userAge = prompt('please enter your age');
+
+if(userAge != null){
 let earlyYears = 10.5;
-let laterYears = myAge - 2;
+let laterYears = userAge - 2;
 laterYears *= 4;
 let myAgeInDogYears = laterYears + earlyYears;
-let myName = 'Farnoud';
-myName = myName.toLowerCase();
-console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
+userName = userName.toLowerCase();
+document.getElementById("test").innerHTML = `Your name is ${userName}. you are ${userAge} years old in human years which is ${myAgeInDogYears} years old in dog years`;
+}else{
+    document.getElementById("test").innerHTML = 'please enter valid value';
+}
+
